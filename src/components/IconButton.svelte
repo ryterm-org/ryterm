5f69430ca5eefff856786744ad580664ae4f643f
AuryChar <nat4nz007love@gmail.com>
2025-11-08 14:31:39 -0300

    Icon button component added

---
+++ src/components/IconButton.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { icon, label } = $props()
+</script>
+
+<button title={label} class="cursor-pointer flex justify-center p-2 rounded-xl items-center border hover:bg-neutral-200 dark:hover:bg-neutral-800 border-neutral-300 dark:border-neutral-700">
+	<i class={`bxr  bx-${icon}`}></i> 
+</button>
